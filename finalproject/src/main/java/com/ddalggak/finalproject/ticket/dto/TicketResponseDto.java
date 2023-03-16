cc89a747ae2b89c2510b6448326877d84280d765
aihtnyc_h <feiver@naver.com>
2023-03-16 15:38:32 +0900

    fix: Ticket 기본 세팅

--- finalproject/src/main/java/com/ddalggak/finalproject/ticket/dto/TicketResponseDto.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/ticket/dto/TicketResponseDto.java
@@ -1,4 +1,3 @@
-package main.java.com.ddalggak.finalproject.ticket.dto;
 package com.ddalggak.finalproject.ticket.dto;
 
 public class TicketResponseDto {
--- finalproject/src/main/java/com/ddalggak/finalproject/ticket/repository/TicketRepository.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/ticket/repository/TicketRepository.java
@@ -1,8 +1,4 @@
-package main.java.com.ddalggak.finalproject.ticket.repository;
->>>>>>> e432a85 (feat: Ticket Repository 기본 세팅)
-=======
-package main.java.com.ddalggak.finalproject.ticket.repository;
->>>>>>> 45919ff (feat: Ticket Repository 기본 세팅)
+package com.ddalggak.finalproject.ticket.repository;
 
 public class TicketRepository {
 }
