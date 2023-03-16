8a884654dbead9f73e01c1fa282c0c5ceebb1f08
aihtnyc_h <feiver@naver.com>
2023-03-16 15:08:12 +0900

    feat: Ticket Repository 기본 세팅

--- finalproject/src/main/java/com/ddalggak/finalproject/ticket/repository/TicketRepository.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/ticket/repository/TicketRepository.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 package com.ddalggak.finalproject.ticket.repository;
+=======
+package main.java.com.ddalggak.finalproject.ticket.repository;
+>>>>>>> e432a85 (feat: Ticket Repository 기본 세팅)
 
 public class TicketRepository {
 }
