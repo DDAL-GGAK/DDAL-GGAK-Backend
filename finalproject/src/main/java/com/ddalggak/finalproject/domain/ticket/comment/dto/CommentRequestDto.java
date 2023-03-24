b9490d20aa5478ebdbf4385242014744c748f266
aihtnyc_h <feiver@naver.com>
2023-03-24 17:13:34 +0900

    fix : Fix comment 수정

--- finalproject/src/main/java/com/ddalggak/finalproject/domain/ticket/comment/dto/CommentRequestDto.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/domain/ticket/comment/dto/CommentRequestDto.java
@@ -1,11 +1,32 @@
-// package com.ddalggak.finalproject.domain.ticket.comment.dto;
-//
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-//
-// @Getter
-// @NoArgsConstructor
-// public class CommentRequestDto{
-// 	public CommentRequestDto(String comments) { this.comments = comments; }
-// 	private String comments;
-// }
+package com.ddalggak.finalproject.domain.ticket.comment.dto;
+
+import java.time.LocalDateTime;
+
+import com.ddalggak.finalproject.domain.ticket.comment.entity.Comment;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CommentRequestDto{
+	private Long commentId;
+	private String comment;
+	// private String description;
+	private String email;
+	private LocalDateTime createdAt;
+	// private LocalDateTime modifiedAt;
+	// private User user;
+
+	@Builder
+	public CommentRequestDto(Comment c) {
+		this.commentId = c.getCommentId();
+		this.comment = c.getComment();
+		// this.description = getDescription();
+		this.email = c.getUser().getEmail();
+		this.createdAt = c.getCreatedAt();
+		// this.modifiedAt = c.getModifiedAt();
+		// this.user = c.getUser();
+	}
+}
--- finalproject/src/main/java/com/ddalggak/finalproject/domain/ticket/entity/Ticket.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/domain/ticket/entity/Ticket.java
@@ -1,10 +1,7 @@
 package com.ddalggak.finalproject.domain.ticket.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -15,17 +12,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.ddalggak.finalproject.domain.task.entity.Task;
-import com.ddalggak.finalproject.domain.ticket.comment.entity.Comment;
 import com.ddalggak.finalproject.domain.ticket.dto.TicketRequestDto;
-import com.ddalggak.finalproject.domain.ticket.dto.TicketResponseDto;
 import com.ddalggak.finalproject.domain.user.entity.User;
 import com.ddalggak.finalproject.global.entity.BaseEntity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
@@ -54,44 +50,71 @@ public class Ticket extends BaseEntity {
 	// 마감 날짜  null 허용 -> 최신 생성순으로
 	@Column(nullable = true)
 	private LocalDateTime expiredAt;
+	@Setter
+	private String taskLeader;
+	@Setter
+	private String teamLeader;
+
 	// task 연관관계
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "taskId")
 	private Task task;
-	// user 연관관계 // FE에서 user -> onwer 로 변경요청
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "userId")
-	private User user;
 
-	@Column(nullable = false)
+	// user 연관관계 // FE에서 user -> onwer 로 변경요청
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "userId")
+	// private User userList;
+
+	@Column(nullable = true)
 	@Enumerated(value = EnumType.STRING)
 	private TicketStatus status;
 	// @OneToMany(mappedBy = "ticket")
 	// private List<User> User = new ArrayList<>();
 
-	// label 연관관계 //티켓에 라벨이 필요한가? // 단방향으로 연관관계? 양방향?
+	//label 연관관계 //티켓에 라벨이 필요한가? // 단방향으로 연관관계? 양방향?
 	// @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE)
 	// private List<Comment> comment = new ArrayList<>();
 
-	public Ticket(TicketResponseDto ticketResponseDto, User user, TicketStatus status) {
-		this.ticketTitle = ticketResponseDto.getTicketTitle();
-		this.ticketDescription = ticketResponseDto.getTicketDescription();
-		this.priority = ticketResponseDto.getPriority();
-		this.difficulty = ticketResponseDto.getDifficulty();
-		this.assigned = ticketResponseDto.getAssigned();
-		this.expiredAt = ticketResponseDto.getExpiredAt();
-		this.user = user;
-		this.status = status;
-		// this.comment = ticketResponseDto.getComments();
-	}
-	public void update(TicketRequestDto ticketRequestDto, TicketStatus status) {
+	@Builder
+	public Ticket(TicketRequestDto ticketRequestDto, User user) {
 		this.ticketTitle = ticketRequestDto.getTicketTitle();
 		this.ticketDescription = ticketRequestDto.getTicketDescription();
 		this.priority = ticketRequestDto.getPriority();
 		this.difficulty = ticketRequestDto.getDifficulty();
 		this.assigned = ticketRequestDto.getAssigned();
-		this.expiredAt = ticketRequestDto.getExpiredAt();
-		this.status = status;
-		// this.user = user;
+		// this.expiredAt = ticketRequestDto.getExpiredAt();
+		this.taskLeader = user.getEmail();
+		// this.addTask(task);
+		this.status = ticketRequestDto.getStatus();
+		this.teamLeader = user.getEmail();
+		// this.comment = ticketResponseDto.getComments();
 	}
+	public void update(TicketRequestDto ticketRequestDto, User user) {
+		this.ticketTitle = ticketRequestDto.getTicketTitle();
+		this.ticketDescription = ticketRequestDto.getTicketDescription();
+		this.priority = ticketRequestDto.getPriority();
+		this.difficulty = ticketRequestDto.getDifficulty();
+		this.assigned = ticketRequestDto.getAssigned();
+		// this.expiredAt = ticketRequestDto.getExpiredAt();
+		// this.addTask(task);
+		this.status = ticketRequestDto.getStatus();
+		this.taskLeader = user.getEmail();
+		this.teamLeader = user.getEmail();
+	}
+	@Builder
+	public static Ticket create(TicketRequestDto ticketRequestDto, User user, Task task) {
+		return Ticket.builder()
+			.ticketRequestDto(ticketRequestDto)
+			.user(user)
+			// .task(task)
+			.build();
+	}
+	// private void addTicketUser(TaskUser taskUser) {
+	// 	taskUser.addTask(taskUser.getTask());
+	// 	task.addTicket(this);
+	// }
+	// private void addTask(Task task) {
+	// 	this.task = task;
+	// 	task.addTicket(this);
+	// }
 }
