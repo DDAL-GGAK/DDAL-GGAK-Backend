5906a94b1424d00806849c42b87c1578d5f6f953
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-24 17:30:41 +0900

    fix: Changed endpoints and requestBody, add invite method #82

--- finalproject/src/main/java/com/ddalggak/finalproject/domain/task/dto/TaskRequestDto.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/domain/task/dto/TaskRequestDto.java
@@ -2,14 +2,19 @@ package com.ddalggak.finalproject.domain.task.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class TaskRequestDto {
+
+	@Schema(name = "project Id", example = "project Id")
+	@NotNull(message = "project Id is required")
+	public Long projectId;
 	@Schema(name = "task title", example = "task title")
 	public String taskTitle;
-
 	@Schema(name = "when does this project expired at", example = "2023-03-22")
 	public LocalDate expiredAt;
 }
