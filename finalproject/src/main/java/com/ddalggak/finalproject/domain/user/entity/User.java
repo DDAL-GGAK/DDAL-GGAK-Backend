349838a0b8b3fe9582ab7e4e7c0a145f778d1744
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-24 17:31:50 +0900

    fix: Add labelUserList into User Entity #82

--- finalproject/src/main/java/com/ddalggak/finalproject/domain/user/entity/User.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/domain/user/entity/User.java
@@ -7,14 +7,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.ddalggak.finalproject.domain.label.entity.LabelUser;
 import com.ddalggak.finalproject.domain.project.entity.ProjectUser;
 import com.ddalggak.finalproject.domain.task.entity.TaskUser;
 import com.ddalggak.finalproject.domain.user.role.UserRole;
@@ -51,25 +49,21 @@ public class User {
 	@Column
 	@Enumerated(value = EnumType.STRING)
 	private UserRole role;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "labelId")
-	private Label label;
-
 	@OneToMany(mappedBy = "user")
 	List<ProjectUser> projectUserList = new ArrayList<>();
 
 	@OneToMany(mappedBy = "user")
 	List<TaskUser> taskUserList = new ArrayList<>();
 
-	public User(Long userId, String email, String nickname, String password, String profile, UserRole role,
-		Label label) {
+	@OneToMany(mappedBy = "user")
+	List<LabelUser> labelUserList = new ArrayList<>();
+
+	public User(Long userId, String email, String nickname, String password, String profile, UserRole role) {
 		this.userId = userId;
 		this.email = email;
 		this.nickname = nickname;
 		this.password = password;
 		this.profile = profile;
 		this.role = role;
-		this.label = label;
 	}
 }
