b34608d18aa5c6290e97b3f080ae7cf67139adb1
Cho Youngjun <whdudwns7777@gmail.com>
2023-03-27 19:25:55 +0900

    fix: Changed directory root

--- finalproject/src/main/java/com/ddalggak/finalproject/domain/randomCode/RandomCodeDto.java
+++
@@ -1,9 +0,0 @@
-package com.ddalggak.finalproject.domain.randomCode;
-
-import lombok.Getter;
-
-@Getter
-public class RandomCodeDto {
-	private String email;
-	private String randomCode;
-}
--- finalproject/src/main/java/com/ddalggak/finalproject/domain/randomCode/RandomCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ddalggak.finalproject.domain.randomCode;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RandomCodeRepository extends CrudRepository<RandomCode, Long> {
-	RandomCode findByRandomCode(String randomCode);
-}
