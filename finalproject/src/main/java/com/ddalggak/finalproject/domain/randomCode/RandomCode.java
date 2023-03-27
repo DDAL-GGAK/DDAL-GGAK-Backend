2df6adc651257b85ce2d701e1065702870079674
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-03-27 09:53:01 +0900

    fix: Redis entity에 맞춰 수정

--- finalproject/src/main/java/com/ddalggak/finalproject/domain/randomCode/RandomCode.java
+++ finalproject/src/main/java/com/ddalggak/finalproject/domain/randomCode/RandomCode.java
@@ -1,10 +1,8 @@
 package com.ddalggak.finalproject.domain.randomCode;
 
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 import lombok.Getter;
@@ -15,10 +13,7 @@ import lombok.NoArgsConstructor;
 @RedisHash(value = "refreshToken", timeToLive = 60 * 5 * 1000L)
 public class RandomCode {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-
-	@Column(name = "email", nullable = false, unique = true)
+	@Column(nullable = false, unique = true)
 	private String email;
 
 	@Column(nullable = false)
